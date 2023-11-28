6d96f9764acdf2116847bd84552d5c3a37de975f
abdiwoli <abdiwolih@gmail.com>
2023-11-29 00:21:03 +0300

    done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
 fr = ARGV[0].scan(/\[from:(\w{4,15})\]/).flatten.join(" ")
-fr += " " + ARGV[0].scan(/\+\d{8,16}/).join(" ") + " "
+fr += "," + ARGV[0].scan(/\+\d{8,16}/).join(" ") + ","
 puts fr +  ARGV[0].scan(/\[flags:(.*?)\]/).flatten.join(" ")
-puts "$"
-puts "$"
