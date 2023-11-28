c80bc36ba50535e1100fb130d6262323a51c0f37
abdiwoli <abdiwolih@gmail.com>
2023-11-29 00:20:04 +0300

    done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,6 +2,5 @@
 fr = ARGV[0].scan(/\[from:(\w{4,15})\]/).flatten.join(" ")
 fr += " " + ARGV[0].scan(/\+\d{8,16}/).join(" ") + " "
 puts fr +  ARGV[0].scan(/\[flags:(.*?)\]/).flatten.join(" ")
-
-
-
+puts "$"
+puts "$"
