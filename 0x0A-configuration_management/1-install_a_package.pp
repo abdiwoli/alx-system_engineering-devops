6b1b13e809893037da4122dd5fbe616a8bff57d9
abdiwoli <abdiwolih@gmail.com>
2024-01-19 14:38:23 -0800

    donedone1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # install flask using puppet
-exec { 'install_flask_2.1.0':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/bin', '/usr/bin'],
-    unless  => '/usr/bin/pip3 show Flask | grep "Version == 2.1.0"',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
-
