fac1229cba7629fe7ae36882841de753e6b45820
abdiwoli <abdiwolih@gmail.com>
2024-01-19 19:00:39 -0800

    done

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 # Puppet Exec Resource to Kill a Process
 exec { 'killmenow':
-  command => 'pkill killmenow',
+  command => 'pkill -9 -f  killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
