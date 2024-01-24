d557498cd907b9deeca0888b4fb238d44c6f8c17
abdiwoli <abdiwolih@gmail.com>
2024-01-24 23:50:12 +0300

    done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,15 @@
 # 100-puppet_ssh_config.pp
+include stdlib
 
 file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/sshd_config',
     line   => 'PasswordAuthentication no',
+    replace => true,
     }
 
 file_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/school',
+    replace => true,
     }
     
