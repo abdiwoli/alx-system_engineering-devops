d035d1d237c1ed8f07fbf8a8ea1e6690bf4f0a2b
abdiwoli <abdiwolih@gmail.com>
2024-01-30 04:50:53 +0300

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,44 @@
-# 2-puppet_custom_http_response_header.pp
+#!/usr/bin/env bash
+# Installs HAProxy version 1.8 with the following configurations:
+#+    Enables management via the init script.
+#+    Distributes requests using a round-robin algorithm.
 
-# Ensure Nginx is installed and the service is running
-class { 'nginx':
-  ensure => 'installed',
-  enable => true,
-  status => 'running',
-}
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:vbernat/haproxy-1.8
+apt-get update
+apt-get install -y haproxy=1.8.\*
 
-# Define a custom fact to get the hostname
-$server_hostname = $facts['hostname']
+echo "ENABLED=1" >> /etc/default/haproxy
+mv /etc/haproxy/haproxy.cfg{,.original}
+touch /etc/haproxy/haproxy.cfg
 
-# Define the Nginx configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
-}
+printf %s "global
+    log 127.0.0.1 local0 notice
+    maxconn 2000
+    user haproxy
+    group haproxy
 
-# Define an ERB template for the Nginx configuration file
-# This template sets up the custom HTTP response header
-file { '/etc/nginx/sites-available/default.erb':
-  ensure  => file,
-  content => '# nginx default configuration file (template)',
-}
+defaults
+    log     global
+    mode    http
+    option  httplog
+    option  dontlognull
+    retries 3
+    option redispatch
+    timeout connect  5000
+    timeout client  10000
+    timeout server  10000
 
-# Define the Nginx service
-service { 'nginx':
-  ensure => 'running',
-}
-k
+listen hbnb
+    bind 0.0.0.0:80
+    mode http
+    stats enable
+    stats uri /haproxy?stats
+    balance roundrobin
+    option httpclose
+    option forwardfor
+    server 375-web-01 104.196.168.90:80 check
+    server 375-web-02 35.196.46.172:80 check
+" >> /etc/haproxy/haproxy.cfg
+
+service haproxy start
