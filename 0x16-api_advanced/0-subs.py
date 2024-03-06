fa29e29dbd3732bb429fefbe007d14eead7115ca
abdiwoli <abdiwolih@gmail.com>
2024-03-06 13:33:12 +0300

    done0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-""" module """
+""" Module for task 0 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers to the subreddit.
-    If the subreddit is not found or an error occurs, returns 0.
-    """
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        headers = {'User-Agent': 'CustomUserAgent/1.0'}
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            subreddit_info = response.json()
-            subscribers_count = subreddit_info.get('data', {}).get('subscribers', 0)
-            return subscribers_count
-        else:
-            print(f"Error: {response.status_code} - {response.text}")
+    """ Queries the Reddit API of the users"""
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
 
-    except Exception as e:
-        print(f"Error: {e}")
-    return 0
+    return sub_info.json().get("data").get("subscribers")
