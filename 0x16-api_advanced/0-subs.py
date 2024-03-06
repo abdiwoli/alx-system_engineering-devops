66da19b77fc77504d9642a35c3589bfa987290e9
abdiwoli <abdiwolih@gmail.com>
2024-03-06 13:16:08 +0300

    done0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module """
+import requests
+
+def number_of_subscribers(subreddit):
+    """ number of subscribers """
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'CustomUserAgent/1.0'}
+    try:
+        response = requests.get(api_url, headers=headers)
+        if response.status_code == 200:
+            subreddit_info = response.json()
+            return subreddit_info['data']['subscribers']
+        elif response.status_code == 404:
+            return 0
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
