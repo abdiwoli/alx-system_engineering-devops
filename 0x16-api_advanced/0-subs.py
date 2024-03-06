94a8f58dd85e648071faf0dd4ca4945c850499ef
abdiwoli <abdiwolih@gmail.com>
2024-03-06 13:37:37 +0300

    done0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API of the users"""
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
-    return sub_info.json().get("data").get("subscribers")
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
