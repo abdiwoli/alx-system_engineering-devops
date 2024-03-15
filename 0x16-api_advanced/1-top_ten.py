4842c6f4ea15a01cdcdccc7d4263909cc125f7c0
abdiwoli <abdiwolih@gmail.com>
2024-03-15 07:28:27 +0300

    hot_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     headers = {
         'User-Agent': 'My User Agent 1.0',
     }
-    subreddit_data = ""
+    subreddit_data = {}
     try:
         res = requests.get('https://www.reddit.com/r/'+subreddit
                            + '/hot.json?limit=10', headers=headers,
