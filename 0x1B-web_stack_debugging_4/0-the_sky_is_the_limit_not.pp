751c6f58c01579c02d98c1a55ec092d1c6c66d9e
abdiwoli <abdiwolih@gmail.com>
2024-03-15 05:45:06 +0300

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,10 @@
 # trying to debug the server
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+exec { 'Adjust Nginx config':
+  command => '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx',
+}
+
+exec { 'Restart Nginx':
+  command     => '/usr/bin/env service nginx restart',
+  refreshonly => true,
+  subscribe   => Exec['Adjust Nginx config'],
+}
