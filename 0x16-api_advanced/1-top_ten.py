78d899d66222b6958faaee0948174d7e73c8f7a5
abdiwoli <abdiwolih@gmail.com>
2024-03-15 08:04:15 +0300

    to+ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """" to ten """
-import json.decoder
 import requests
 
 
@@ -12,14 +11,19 @@ def top_ten(subreddit):
     try:
         res = requests.get('https://www.reddit.com/r/' + subreddit + '/hot.json?limit=10', headers=headers, allow_redirects=False)
         subreddit_data = res.json()
-        if 'error' in subreddit_data.keys():
+
+        if 'error' in subreddit_data:
             print('None')
         else:
-            more = subreddit_data['data']['children']
-            for detail in more:
-                print(detail['data']['title'])
+            data = subreddit_data.get('data', {})
+            children = data.get('children', [])
+            for post in children:
+                post_data = post.get('data', {})
+                title = post_data.get('title')
+                if title:
+                    print(title)
+                else:
+                    print("Error: Missing 'title' field in post data")
 
-    except json.decoder.JSONDecodeError:
-        print('None')
     except Exception as e:
         print('None')
