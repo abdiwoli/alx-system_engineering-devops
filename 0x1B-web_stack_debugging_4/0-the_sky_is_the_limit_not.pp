48001bf376adddaad7c231863b51efa4a07c8d78
abdiwoli <abdiwolih@gmail.com>
2024-03-15 05:57:02 +0300

    done

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,21 @@
-# trying to debug the server
-# Puppet script to adjust the maximum number of open files for the holberton user
+# Puppet script to adjust the OS configuration for the holberton user
 
 file_line { 'Increase open file limit for holberton user':
   path    => '/etc/security/limits.conf',
-  line    => '*                soft    nofile          1024',
+  line    => 'holberton hard nofile 1024',
   ensure  => present,
-  match   => '^*.*holberton.*nofile',
 }
 
-file_line { 'Increase open file limit for holberton user (hard limit)':
-  path    => '/etc/security/limits.conf',
-  line    => '*                hard    nofile          1024',
-  ensure  => present,
-  match   => '^*.*holberton.*nofile',
-}
-
-file_line { 'Increase open file limit for session':
+file_line { 'Include pam_limits module':
   path    => '/etc/pam.d/common-session',
-  line    => 'session required        pam_limits.so',
+  line    => 'session required pam_limits.so',
   ensure  => present,
-  match   => '^session.*pam_limits.so',
 }
 
-exec { 'Restart PAM service':
-  command     => '/bin/systemctl restart systemd-logind',
-  refreshonly => true,
+service { 'restart-login':
+  name    => 'systemd-logind',
+  ensure  => 'running',
+  enable  => true,
+  subscribe => File_line['Increase open file limit for holberton user', 'Include pam_limits module'],
 }
+k
