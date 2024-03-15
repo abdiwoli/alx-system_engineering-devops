62025fe63e5db7b6da43f2abe58fc07969ca0f7b
abdiwoli <abdiwolih@gmail.com>
2024-03-15 08:44:53 +0300

    done

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module for task 0 """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ Queries the Reddit API of the users"""
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers", 0)
+    else:
+        return 0
